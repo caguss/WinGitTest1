fea2191e298cc43b8c1f9827be003abdca1738fb
caguss <dipsy1014@naver.com>
2019-12-24 15:05:19 +0900

    버튼2 추가

--- WinGitTest1/Form1.cs
+++ WinGitTest1/Form1.cs
@@ -21,5 +21,10 @@ namespace WinGitTest1
         {
             this.Text = "버튼클릭";
         }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            this.Text = "qfqffeafsefaefaef";
+        }
     }
 }
